062deda8bf68cedeea2012da94240196026501bb
Hiromichi_hiroiwa <Hiromichi.Hiroiwa@gmail.com>
2022-06-25 00:18:26 +0900

    認証系削除

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
-import { AuthController } from '@controllers/auth.controller';
 import { IndexController } from '@controllers/index.controller';
 import { UsersController } from '@controllers/users.controller';
 
 import App from '@/app';
 
 try {
-  const app = new App([AuthController, IndexController, UsersController]);
+  const app = new App([IndexController, UsersController]);
   app.listen();
 } catch (error) {
   console.log(error)
